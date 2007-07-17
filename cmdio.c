2d9167c110abbed3e5f10e4659fdbc02ad9b489d
Lucian Adrian Grijincu <lucian.grijincu@gmail.com>
2007-07-18 02:37:16 +0300

    added cmdio and ftpcodes

---
+++ cmdio.c
@@ -0,0 +1,8 @@
+#include "cmdio.h"
+
+
+apr_status_t lfd_cmdio_write(struct lfd_sess * p_sess, int cmd, const char *msg, ...)
+{
+	return APR_SUCCESS;
+}
+
---
+++ cmdio.h
@@ -0,0 +1,9 @@
+#ifndef LKLFTPD_CMDIO_H__
+#define LKLFTPD_CMDIO_H__
+
+#include <apr_errno.h>
+#include <apr_errno.h>
+struct lfd_sess;
+apr_status_t lfd_cmdio_write(struct lfd_sess * p_sess, int cmd, const char *msg, ...);
+#endif//LKLFTPD_CMDIO_H__
+
